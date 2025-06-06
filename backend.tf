540c8e86f1cb0c2d33b280d9429a58f0125892e9
nag9844 <96878041+nag9844@users.noreply.github.com>
2025-06-06 05:27:52 +0000

    update workflow

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "usecases-terraform-state-bucket"
-    key          = "usecase3-4/statefile.tfstate"
+    key          = "usecase6/statefile.tfstate"
     region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
